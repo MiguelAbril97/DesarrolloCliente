4bf2019178b241cf97ba4adf865e07bda0d88183
Miguel Abril <miguel.abril.alamo.al@iespoligonosur.org>
2024-09-27 10:46:56 +0200

    Hasta ejercicio 16 y creo la carpeta Boletin1

---
+++ Boletin 1/ej16.js
@@ -0,0 +1,31 @@
+let anyo1=0;
+let mes1=0;
+let dia1=0;
+let anyo2=0;
+let mes2=0;
+let dia2=0;
+let entrada;
+
+entrada=prompt("Introduce el año de la primera fecha ");
+anyo1=parseInt(entrada);
+
+entrada=prompt("Introduce el mes de la primera fecha en números ");
+mes1=parseInt(entrada);
+
+entrada=prompt("Introduce el día de la primera fecha ");
+dia1=parseInt(entrada);
+
+//Segunda fecha
+
+entrada=prompt("Introduce el año de la segunda fecha ");
+anyo2=parseInt(entrada);
+
+entrada=prompt("Introduce el mes de la segunda fecha en números ");
+mes2=parseInt(entrada);
+
+entrada=prompt("Introduce el día de la segunda fecha ");
+dia2=parseInt(entrada);
+
+
+//Calculo
+
